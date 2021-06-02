71e9c0dd6f5530e71db429ad665130f0dd49e20b
devGeanina <35954631+devGeanina@users.noreply.github.com>
2021-06-02 12:22:00 +0300

    Update LoginDAOImpl.java
    Remove comments.

--- src/main/java/com/blocadmin/core/dao/LoginDAOImpl.java
+++ src/main/java/com/blocadmin/core/dao/LoginDAOImpl.java
@@ -13,10 +13,8 @@ public class LoginDAOImpl extends CommonDAOImpl<User> implements LoginDAO {
 		TypedQuery<User> q = em
 				.createQuery("SELECT u FROM User u WHERE LOWER(username)=:username AND LOWER(password)=:password",
 						User.class);
-				//.setHint("org.hibernate.cacheable", true);
 		q.setParameter("username", user.toLowerCase());
 		q.setParameter("password", password.toLowerCase());
-		//q.setParameter("userType", Constants.USER_TYPE.ADMIN.getType());
 		List<User> users = q.getResultList();
 		if (users.size() == 1) {
 			return users.get(0);
